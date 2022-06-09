fa62b9fc4320a1003c811482dad2663f2e6e98dc
hiYifang <a6426320@gmail.com>
2022-06-09 23:18:38 +0800

    feat: 新增按讚、追蹤功能

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -5,12 +5,46 @@ const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const { getJWT } = require('../service/auth');
 
+const mongoose = require('mongoose');
 const User = require('../models/usersModel');
-const Follow = require('../models/followsModel');
+const Post = require('../models/postsModel');
 
 const users = {
+  // 取得個人資料
+  getProfile: handleErrorAsync(async (req, res) => {
+    const { user } = req;
+    res.status(200).json(getHttpResponse(user));
+  }),
+  // 取得個人按讚列表
+  getLikeList: handleErrorAsync(async (req, res) => {
+    const { user } = req;
+    const likeList = await Post.find({
+      likes: { $in: [user.id] }
+    }).populate({
+      path: "user",
+      select: "_id nickName"
+    }).select('-messages')
+      .sort({
+        createdAt: -1,
+      });
+    res.status(200).json(getHttpResponse(likeList));
+  }),
+  // 取得個人追蹤名單
+  getFollowList: handleErrorAsync(async (req, res) => {
+    const { user } = req;
+    const following = await User.find({ user: user._id })
+      .populate({ path: 'following.user', select: 'nickName avatar' });
+    res.status(200).json(getHttpResponse(following));
+  }),
+  // 註冊會員
   signUp: handleErrorAsync(async (req, res, next) => {
-    let { email, password, nickName } = req.body;
+    const { 
+      body: { 
+        email,
+        password,
+        nickName
+      }
+    } = req;
     const emailExist = await User.findOne({ email });
 
     if (!nickName) {
@@ -45,13 +79,19 @@ const users = {
       password,
       nickName
     });
-    res.status(201).json(getHttpResponse({ 
+    res.status(201).json(getHttpResponse({
       token: getJWT(newUser),
       id: newUser._id
     }));
   }),
+  // 登入會員
   signIn: handleErrorAsync(async (req, res, next) => {
-    const { email, password } = req.body;
+    const { 
+      body: { 
+        email,
+        password 
+      }
+    } = req;
 
     if (!email) {
       return next(appError(400, "登入失敗，請重新填寫 Email 欄位", "email"))
@@ -80,13 +120,60 @@ const users = {
     if (!auth) {
       return next(appError(400, "登入失敗，密碼不正確", "password"))
     }
-    res.status(201).json(getHttpResponse({ 
+    res.status(201).json(getHttpResponse({
       token: getJWT(user),
-      id: user._id 
+      id: user._id
     }));
   }),
+  // 追蹤朋友
+  postFollow: handleErrorAsync(async (req, res, next) => {
+    const {
+      user,
+      params: { userID }
+    } = req;
+
+    if (!(userID && mongoose.Types.ObjectId.isValid(userID))) {
+      return next(appError(400, "請傳入特定的追蹤會員", "userID"))
+    }
+
+    if (userID === user.id) {
+      return next(appError(400, "無法追蹤自己", "userID"))
+    }
+
+    const existedUser = await User.findById(userID);
+    if (!existedUser) {
+      return next(appError(400, "追蹤的會員尚未註冊", "userID"))
+    }
+
+    await User.updateOne(
+      {
+        _id: user.id,
+        'following.user': { $ne: userID }
+      },
+      {
+        $addToSet: { following: { user: userID } }
+      }
+    );
+    await User.updateOne(
+      {
+        _id: userID,
+        'followers.user': { $ne: user.id }
+      },
+      {
+        $addToSet: { followers: { user: user.id } }
+      }
+    );
+    res.status(200).json(getHttpResponse({ message: '已追蹤！' }));
+  }),
+  // 重設密碼
   updatePassword: handleErrorAsync(async (req, res, next) => {
-    const { user, body: { password, confirmPassword } } = req;
+    const { 
+      user, 
+      body: { 
+        password,
+        confirmPassword 
+      }
+    } = req;
 
     if (!password) {
       return next(appError(400, "設定失敗，請填寫 Password 欄位", "password"))
@@ -108,11 +195,16 @@ const users = {
     await User.updateOne({ _id: user._id }, { password: newPassword });
     res.status(201).json(getHttpResponse({ message: "更新密碼成功" }));
   }),
-  getProfile: handleErrorAsync(async (req, res) => {
-    res.status(200).json(getHttpResponse(req.user));
-  }),
+  // 更新個人資料
   patchProfile: handleErrorAsync(async (req, res, next) => {
-    const { user, body: { nickName, gender, avatar } } = req;
+    const { 
+      user, 
+      body: { 
+        nickName, 
+        gender, 
+        avatar 
+      } 
+    } = req;
 
     if (!nickName) {
       return next(appError(400, "更新失敗，請填寫暱稱欄位", "nickName"))
@@ -126,6 +218,46 @@ const users = {
 
     await User.findByIdAndUpdate(user._id, { nickName, gender, avatar });
     res.status(201).json(getHttpResponse({ message: "更新個人資料成功" }));
+  }),
+  // 取消追蹤朋友
+  delFollow: handleErrorAsync(async (req, res, next) => {
+    const {
+      user,
+      params: { userID }
+    } = req;
+
+    if (!(userID && mongoose.Types.ObjectId.isValid(userID))) {
+      return next(appError(400, "請傳入特定的追蹤會員", "userID"))
+    }
+
+    if (userID === user.id) {
+      return next(appError(400, "無法追蹤自己", "userID"))
+    }
+
+    const existedUser = await User.findById(userID);
+    if (!existedUser) {
+      return next(appError(400, "追蹤的會員尚未註冊", "userID"))
+    }
+
+    await User.updateOne(
+      {
+        _id: user.id
+      },
+      {
+        $pull: { following: { user: userID } }
+      }
+    );
+
+    await User.updateOne(
+      {
+        _id: userID
+      },
+      {
+        $pull: { followers: { user: user.id } }
+      }
+    );
+
+    res.status(200).json(getHttpResponse({ message: '已取消追蹤！' }));
   })
 }
 
