5c66f8fe35977a4983b704ad3a2110ab5c1d662a
hiYifang <a6426320@gmail.com>
2022-06-09 23:09:18 +0800

    feat: 安裝套件 - 流量限制

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const rateLimit = require("express-rate-limit");
 
 const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/* 流量限制 */
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 分鐘
+  max: 1000
+});
+app.use(globalLimiter);
 
 app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
